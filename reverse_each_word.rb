910dc63a15670496d1ee712dd849fd2bca674ed0
adventurous-format-6947 <bminczu@gmail.com>
2020-10-12 16:12:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,10 @@
 def reverse_each_word(string)
 string.split.collect {|word| word.reverse}.join(" ")
 end
+
+def  reverse_each_word(string)
+  memo = " "
+  sentence1.split.each { |joined_array| memo << "#{joined_array.reverse} " }
+  p memo.rstrip
+
+end
